c06bdb7a9cf33ce3f8edad37113a3ab76d70d76e
Mike Maslyaev <miklemas@list.ru>
2023-12-29 18:11:54 +0300

    Added loader file

--- epde/loader.py
+++ epde/loader.py
@@ -13,12 +13,16 @@ from epde.structure.main_structures import SoEq
 from epde.interface.token_family import TFPool
 from epde.optimizers.moeadd.moeadd import ParetoLevels
 from epde.optimizers.single_criterion.optimizer import Population
-from epde.cache import cache
+from epde.cache.cache import Cache
 
 class EPDELoader(object):
     '''
-    Universal loader for EPDE objects, associated with 
+    Universal loader for EPDE objects, applicable to system of equations as 
+    ``SoEq``, token families pool as ``TFPool``, populations as 
     '''
+    _types = {'SoEq' : SoEq, 'TFPool' : TFPool, 'cache' : Cache,
+             'multiobj_pop' : ParetoLevels, 'singleobj_pop' : Population}    
+    
     def __init__(self, directory = None):
         if directory is not None:
             if not isinstance(directory, str):
@@ -35,19 +39,24 @@ class EPDELoader(object):
             self._directory = os.path.normpath((os.path.join(os.path.dirname(os.getcwd()), 
                                                             '..','epde_cache')))
         
-    def save(self, obj, filename = None):
-        #Make save methods for equations, pool and populations
-        pickling_form = obj.to_pickle()
+    def save(self, obj, filename:str = None, except_attrs:list = []):
         with open(filename, mode = 'wb') as file:
-            pickle.dump(pickling_form, file)
+            pickle.dump(obj.to_pickle(), file)
+    
+    def saves(self, obj):
+        pickling_form = obj.to_pickle()
+        pickle.dumps(pickling_form)        
 
-    def load(self, filename):
-        types = {'equation' : SoEq, 'pool' : TFPool,
-                 'multiobj_pop' : ParetoLevels, 'singleobj_pop' : Population}
-        
-        with open(filename, mode = 'rb') as file:
-            obj_pickled = pickle.load(file)
-        
-        obj = types[obj_pickled['obj_type']].__new__(types[obj_pickled['obj_type']])
+    def use_pickles(self, obj_pickled):
+        obj = self._types[obj_pickled['obj_type']].__new__(self._types[obj_pickled['obj_type']])
         obj.attrs_from_dict(obj_pickled)
         return obj
+
+    def load(self, filename:str, **kwargs):
+        with open(filename, mode = 'rb') as file:
+            obj_pickled = pickle.load(file)
+        return self.use_pickles(obj_pickled)
+
+    def loads(self, byteobj):
+        obj_pickled = pickle.loads(byteobj)
+        return self.use_pickles(obj_pickled)
