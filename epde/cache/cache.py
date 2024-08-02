Fix of gradient tensor construction bug

epde/cache/cache.py
@@ -363,14 +363,12 @@ class Cache(object):
 
     def get(self, label, normalized=False, structural=False, saved_as=None, torch_mode: bool = False, deriv_code = None):
         assert not (normalized and structural), 'The added matrix can not be simultaneously normalized and scaled'
-        # print(f'Called {label} with torch mode {torch_mode}')
         type_key, other, other_bool = ('torch', 'numpy', False) if torch_mode else ('numpy', 'torch', True)
         if deriv_code is not None:
-            print(f'self._deriv_codes {self._deriv_codes} for {deriv_code}')
             label = [elem[1] for elem in self._deriv_codes if elem[0] == deriv_code][0]
 
         if label is None:
-            print(self.memory_default[type_key].keys())
+            # print(self.memory_default[type_key].keys())
             return np.random.choice(list(self.memory_default[type_key].values()))
         if normalized:
             if label not in self.memory_normalized[type_key]:

epde/interface/control_utils.py
@@ -90,13 +90,14 @@ class ConstrLocation():
 
 
 class ControlConstraint(ABC):
-    def __init__(self, val : Union[float, torch.Tensor], deriv_method: BasicDeriv, # grid: torch.Tensor, 
-                 indices: ConstrLocation, deriv_axes: List = [None,], nn_output: int = 0, **kwargs):
+    def __init__(self, val : Union[float, torch.Tensor], deriv_method: BasicDeriv, indices: ConstrLocation,
+                 device: str = 'cpu', deriv_axes: List = [None,], nn_output: int = 0, **kwargs):
         self._val = val
         self._indices = indices
         self._axes = deriv_axes
         self._nn_output = nn_output
         self._deriv_method = deriv_method
+        self._device = device
 
     @abstractmethod
     def __call__(self, fun_nn: Union[torch.nn.Sequential, torch.Tensor], 
@@ -112,10 +113,11 @@ class ControlConstrEq(ControlConstraint):
     Class for constrints of type $c(u^(n)) = f(u) - val = 0$
     '''
     def __init__(self, val : Union[float, torch.Tensor], deriv_method: BasicDeriv, # grid: torch.Tensor, 
-                 indices: ConstrLocation, deriv_axes: List = [None,], 
-                 nn_output: int = 0, tolerance: float = 1e-7):
-        super().__init__(val, deriv_method, indices, deriv_axes, nn_output) # grid, 
+                 indices: ConstrLocation, device: str = 'cpu', deriv_axes: List = [None,], 
+                 nn_output: int = 0, tolerance: float = 1e-7, estim_func: Callable = None):
+        super().__init__(val, deriv_method, indices, device, deriv_axes, nn_output) # grid, 
         self._eps = tolerance
+        self._estim_func = estim_func
  
     def __call__(self, fun_nn: Union[torch.nn.Sequential, torch.Tensor], 
                  arg_tensor: torch.Tensor) -> Tuple[bool, torch.Tensor]:
@@ -127,7 +129,12 @@ class ControlConstrEq(ControlConstraint):
             if to_compare.shape != self._val.shape:
                 raise TypeError(f'Incorrect shapes of constraint value tensor: expected {self._val.shape}, got {to_compare.shape}.')
             val_transformed = self._val
-        return torch.isclose(val_transformed, to_compare, rtol = self._eps), val_transformed - to_compare
+        if self._estim_func is not None:
+            constr_enf = self._estim_func(val_transformed, to_compare)
+        else:
+            constr_enf = val_transformed - to_compare
+        return (torch.isclose(constr_enf, torch.zeros_like(constr_enf).to(self._device), rtol = self._eps), 
+                val_transformed - to_compare)
         
     def loss(self, fun_nn: torch.nn.Sequential, arg_tensor: torch.Tensor) -> torch.Tensor:
         _, discrepancy = self(fun_nn, arg_tensor)
@@ -138,10 +145,11 @@ class ControlConstrNEq(ControlConstraint):
     Class for constrints of type $c(u, x) = f(u, x) - val `self._sign` 0$
     '''
     def __init__(self, val : Union[float, torch.Tensor], deriv_method: BasicDeriv, # grid: torch.Tensor, 
-                 indices: ConstrLocation, sign: str = '>', deriv_axes: List = [None,], 
-                 nn_output: int = 0, tolerance: float = 1e-7):
-        super().__init__(val, deriv_method, indices, deriv_axes, nn_output) # grid, 
+                 indices: ConstrLocation, device: str = 'cpu', sign: str = '>', deriv_axes: List = [None,], 
+                 nn_output: int = 0, tolerance: float = 1e-7, estim_func: Callable = None):
+        super().__init__(val, deriv_method, indices, device, deriv_axes, nn_output) # grid, 
         self._sign = sign
+        self._estim_func = estim_func
 
     def __call__(self, fun_nn: torch.nn.Sequential, arg_tensor: torch.Tensor) -> Tuple[bool, torch.Tensor]:
         to_compare = self._deriv_method.take_derivative(u = fun_nn, args=self._indices.apply(arg_tensor, along_axis=0), # correct along_axis argument 
@@ -151,10 +159,17 @@ class ControlConstrNEq(ControlConstraint):
         else:
             assert to_compare.shape == self._val.shape, 'Incorrect shapes of constraint value tensor'
             val_transformed = self._val
+        
+        if self._estim_func is not None:
+            constr_enf = self._estim_func(val_transformed, to_compare)
+        else:
+            constr_enf = val_transformed - to_compare
+
         if self._sign == '>':
-            return torch.greater(val_transformed, to_compare), torch.nn.functional.relu(val_transformed - to_compare)
+            return torch.greater(constr_enf, torch.zeros_like(constr_enf).to(self._device)), torch.nn.functional.relu(constr_enf)
         elif self._sign == '<':
-            return torch.less(val_transformed, to_compare), torch.nn.functional.relu(to_compare - val_transformed)            
+            return torch.less(constr_enf, torch.zeros_like(constr_enf).to(self._device)), torch.nn.functional.relu(constr_enf)
+        #torch.less(val_transformed, to_compare), torch.nn.functional.relu(to_compare - val_transformed)            
 
         
     def loss(self, fun_nn: torch.nn.Sequential, arg_tensor: torch.Tensor) -> torch.Tensor:
@@ -262,8 +277,8 @@ class ControlExp():
                                         loc = loc, forward=True, eps=eps)
         global_var.control_nn.net.load_state_dict(state_dict)
         state_dict = state_dict_prev = None
-
-        res = (loss_forward - loss_back)/(2*eps)
+        with torch.no_grad():
+            res = (loss_forward - loss_back)/(2*eps)
         return res
         
 
@@ -302,7 +317,7 @@ class ControlExp():
             sampled_bc = [modify_bc(operator, noise_std) for operator, noise_std in bc_operators]
 
             state_net  = deepcopy(self._state_net)
-            eps = 1e-3
+            eps = 1e-5
             print(f'Control function optimization epoch {t}.')
             for param_key, param_tensor in grad_tensors.items():
                 print(f'Optimizing {param_key}: shape is {param_tensor.shape}')
@@ -313,10 +328,10 @@ class ControlExp():
                         grad_tensors[loc[0]] = grad_tensors[loc[0]].detach()
                         grad_tensors[loc[0]][loc[1:]] = self.finite_diff_calculation(system = self.system,
                                                                                      adapter = adapter,
-                                                                                     loc = loc, control_loss = self.loss, 
+                                                                                     loc = loc, control_loss = self.loss,
                                                                                      state_net = self._state_net,
                                                                                      bc_operators = sampled_bc,
-                                                                                     grids = [grids, grids_merged], 
+                                                                                     grids = [grids, grids_merged],
                                                                                      solver_params = self._solver_params,
                                                                                      eps = eps)
                 elif len(param_tensor.size()) == 2:
@@ -324,14 +339,14 @@ class ControlExp():
                         for param_inner_idx, _ in enumerate(param_tensor[0]):
                             loc = (param_key, param_outer_idx, param_inner_idx)
                             grad_tensors[loc[0]] = grad_tensors[loc[0]].detach()
-                            grad_tensors[loc[0]][loc[1:]] = self.finite_diff_calculation(system = self.system,
-                                                                                         adapter = adapter,
-                                                                                         loc = loc, control_loss = self.loss, 
-                                                                                         state_net = self._state_net,
-                                                                                         bc_operators = sampled_bc,
-                                                                                         grids = [grids, grids_merged], 
-                                                                                         solver_params = self._solver_params,
-                                                                                         eps = eps)                   
+                            grad_tensors[loc[0]][tuple(loc[1:])] = self.finite_diff_calculation(system = self.system,
+                                                                                                adapter = adapter,
+                                                                                                loc = loc, control_loss = self.loss,
+                                                                                                state_net = self._state_net,
+                                                                                                bc_operators = sampled_bc,
+                                                                                                grids = [grids, grids_merged],
+                                                                                                solver_params = self._solver_params,
+                                                                                                eps = eps)
                 else:
                     raise Exception(f'Incorrect shape of weights/bias. Got {param_tensor.size()} tensor.')
             state_dict_prev = global_var.control_nn.net.state_dict()
@@ -351,8 +366,9 @@ class ControlExp():
             var_prediction = model(grids_merged)
             self._state_net = model
 
-            control_inputs = prepare_control_inputs(model, grids_merged, global_var.control_nn.net_args)        
-            loss = self.loss([self._state_net, global_var.control_nn.net], [grids_merged, control_inputs])
+            control_inputs = prepare_control_inputs(model, grids_merged, global_var.control_nn.net_args)
+            with torch.no_grad():
+                loss = self.loss([self._state_net, global_var.control_nn.net], [grids_merged, control_inputs])
             print('current loss is ', loss)
             # if loss < min_loss:
             # min_loss = loss

epde/interface/prepared_tokens.py
@@ -191,7 +191,10 @@ class ControlVarTokens(PreparedTokens):
                                       derivs_solver_orders=[der_ords,])
         
         def nn_eval_torch(*args, **kwargs):
-            inp = torch.cat([torch.reshape(tensor, (-1, 1)) for tensor in args], dim = 1) # Validate correctness
+            if isinstance(args[0], torch.Tensor):
+                inp = torch.cat([torch.reshape(tensor, (-1, 1)) for tensor in args], dim = 1) # Validate correctness
+            else:
+                inp = torch.cat([torch.reshape(torch.Tensor([elem,]), (-1, 1)) for elem in args], dim = 1)
             # print(f'passing tensor, stored at {inp.get_device()}')
             # print(f'inp shape is {inp.shape}, args are {args}, kwargs are {kwargs}')
             return global_var.control_nn.net(inp)#**kwargs['power']

projects/control/controlled_LV.py
@@ -212,18 +212,18 @@ def optimize_ctrl(eq: epde.structure.main_structures.SoEq, t: torch.tensor,
 
     loc = control_utils.ConstrLocation(domain_shape = (t.size()[0],), device=device) # Declaring const in the entire domain
     u_tar_constr = control_utils.ControlConstrEq(val = torch.full_like(input = t, fill_value = u_tar, device=device),
-                                                 indices = loc, deriv_method = autograd, nn_output=0)
+                                                 indices = loc, deriv_method = autograd, nn_output=0, device=device)
     v_tar_constr = control_utils.ControlConstrEq(val = torch.full_like(input = t, fill_value = v_tar, device=device),
-                                                 indices = loc, deriv_method = autograd, nn_output=1)
+                                                 indices = loc, deriv_method = autograd, nn_output=1, device=device)
     contr_constr = control_utils.ControlConstrEq(val = torch.full_like(input = t, fill_value = 0., device=device),
-                                                 indices = loc, deriv_method = autograd, nn_output=0)
+                                                 indices = loc, deriv_method = autograd, nn_output=0, device=device)
 
     u_var_non_neg = control_utils.ControlConstrNEq(val = torch.full_like(input = t, fill_value = 0., device=device), sign='>',
-                                                   indices = loc, deriv_method = autograd, nn_output=0)
+                                                   indices = loc, deriv_method = autograd, nn_output=0, device=device)
     v_var_non_neg = control_utils.ControlConstrNEq(val = torch.full_like(input = t, fill_value = 0., device=device), sign='>',
-                                                   indices = loc, deriv_method = autograd, nn_output=1)
+                                                   indices = loc, deriv_method = autograd, nn_output=1, device=device)
     contr_non_neg = control_utils.ControlConstrNEq(val = torch.full_like(input = t, fill_value = 0., device=device), sign='>',
-                                                   indices = loc, deriv_method = autograd, nn_output=0)    
+                                                   indices = loc, deriv_method = autograd, nn_output=0, device=device)
 
     
     loss = control_utils.ConditionalLoss([(1., u_tar_constr, 0),
@@ -254,14 +254,14 @@ def optimize_ctrl(eq: epde.structure.main_structures.SoEq, t: torch.tensor,
 
     optimizer.set_control_optim_params()
 
-    optimizer.set_solver_params(training_params = {'epochs': 100,})
+    optimizer.set_solver_params(training_params = {'epochs': 150,})
 
-    state_nn, ctrl_net, ctrl_pred, hist = optimizer.train_pinn(bc_operators = [(bop_u(device=device), 0.001), 
-                                                                               (bop_v(device=device), 0.001)], 
+    state_nn, ctrl_net, ctrl_pred, hist = optimizer.train_pinn(bc_operators = [(bop_u(device=device), 0.0),
+                                                                               (bop_v(device=device), 0.0)],
                                                                grids = [t,], n_control = 1., 
                                                                state_net = state_nn_pretrained, 
-                                                               opt_params = [0.05, 0.9, 0.999, 1e-8],
-                                                               control_net = ctrl_nn_pretrained, epochs = 30,
+                                                               opt_params = [0.01, 0.9, 0.999, 1e-8],
+                                                               control_net = ctrl_nn_pretrained, epochs = 75,
                                                                fig_folder = fig_folder)
 
     return state_nn, ctrl_net, ctrl_pred, hist
@@ -318,14 +318,14 @@ if __name__ == '__main__':
     args = torch.from_numpy(solution).float().to(device)
 
     def create_shallow_nn(arg_num: int = 1, output_num: int = 1, device = 'cpu') -> torch.nn.Sequential: # net: torch.nn.Sequential = None, 
-        hidden_neurons = 180
+        hidden_neurons = 100
         layers = [torch.nn.Linear(arg_num, hidden_neurons, device=device),
-                  torch.nn.ReLU(),
+                  torch.nn.ReLU(), # ReLU(),
                   torch.nn.Linear(hidden_neurons, output_num, device=device)]
         return torch.nn.Sequential(*layers)
     
     def create_deep_nn(arg_num: int = 1, output_num: int = 1, device = 'cpu') -> torch.nn.Sequential: # net: torch.nn.Sequential = None, 
-        hidden_neurons = 13
+        hidden_neurons = 18
         layers = [torch.nn.Linear(arg_num, hidden_neurons, device=device),
                   torch.nn.Tanh(),
                   torch.nn.Linear(hidden_neurons, hidden_neurons, device=device),
@@ -335,18 +335,20 @@ if __name__ == '__main__':
     
     time_exp_start = datetime.datetime.now()
     
+    nn = 'shallow'
     load_ctrl = False
 
     if device == 'cpu':
-        ctrl_fname = r"/home/mikemaslyaev/Documents/EPDE/projects/control/control_ann_shallow_cpu.pickle"
+        ctrl_fname = f"/home/mikemaslyaev/Documents/EPDE/projects/control/control_ann_{nn}_cpu.pickle"
     else:
-        ctrl_fname = r"/home/mikemaslyaev/Documents/EPDE/projects/control/control_ann_shallow_cuda.pickle"
+        ctrl_fname = f"/home/mikemaslyaev/Documents/EPDE/projects/control/control_ann_{nn}_cuda.pickle"
     if load_ctrl:
         with open(ctrl_fname, 'rb') as ctrl_input_file:  
             ctrl_ann = pickle.load(ctrl_input_file)
     else:
+        nn_method = create_shallow_nn if nn == 'shallow' else create_deep_nn
         ctrl_ann = epde.supplementary.train_ann(args=[solution[:, 0], solution[:, 1]], data = ctrl, 
-                                                epochs_max = 2e4, dim = 2, model = create_deep_nn(2, 1, device=device),
+                                                epochs_max = 2e4, dim = 2, model = nn_method(2, 1, device=device),
                                                 device = device)
 
         with open(ctrl_fname, 'wb') as ctrl_output_file:  

projects/control/swingup.py
@@ -7,24 +7,23 @@ Created on Wed Apr  3 17:43:03 2024
 """
 import os
 import sys
+from collections import OrderedDict
+from typing import List
 import faulthandler
 
-
-
 faulthandler.enable()
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../..')))
 
 import numpy as np
-from tqdm import tqdm
-from collections import OrderedDict
-from typing import List
+import torch
 
+from tqdm import tqdm
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 import epde
+import epde.interface.control_utils as control_utils
 from projects.control.swingup_aux import DMCEnvWrapper, RandomPolicy, CosinePolicy, CosineSignPolicy, \
                                          TwoCosinePolicy, rollout_env, VarTrigTokens # ,, DerivSignFunction
 from epde.interface.prepared_tokens import DerivSignFunction
@@ -34,7 +33,7 @@ def get_additional_token_families(ctrl):
     sgn_tokens = DerivSignFunction(token_type = 'speed_sign', var_name = 'y', token_labels=['sign(dy/dx1)',],
                                    deriv_solver_orders = [[0,],])
     control_var_tokens = epde.interface.prepared_tokens.ControlVarTokens(sample = ctrl, arg_var = [(0, [None,]), (1, [None,]), 
-                                                                                                   (0, [1,]), (1, [1,])])
+                                                                                                   (0, [0,]), (1, [0,])])
     return [angle_trig_tokens, sgn_tokens, control_var_tokens] 
 
 def epde_discovery(t, x, angle, u, derivs, diff_method = 'FD'):
@@ -216,6 +215,71 @@ def translate_equation(t, x, angle, u, derivs: dict, diff_method = 'FD'):
 
     return test
 
+def optimize_ctrl(eq: epde.structure.main_structures.SoEq, t: torch.tensor,
+                  y_left: float, y_right: float, stab_der_ord: int,
+                  state_nn_pretrained: torch.nn.Sequential, ctrl_nn_pretrained: torch.nn.Sequential, 
+                  fig_folder: str, device = 'cpu'):
+    
+    from epde.supplementary import AutogradDeriv
+    autograd = AutogradDeriv()
+
+    loc = control_utils.ConstrLocation(domain_shape = (t.size()[0],), device=device) # Declaring const in the entire domain
+    cosine_cond = lambda x, ref: torch.cos(x) - ref
+    phi_tar_constr = control_utils.ControlConstrEq(val = torch.full_like(input = t[-1], fill_value = 1., device=device), # Better processing for periodic
+                                                   indices = loc, deriv_axes=[None,], deriv_method = autograd, nn_output=0, 
+                                                   estim_func=cosine_cond)
+    dphi_tar_constr = control_utils.ControlConstrEq(val = torch.full_like(input = t[-1], fill_value = 0, device=device),
+                                                    indices = loc, deriv_axes=[0,], deriv_method = autograd, nn_output=1)
+    contr_constr = control_utils.ControlConstrEq(val = torch.full_like(input = t, fill_value = 0., device=device),
+                                                 indices = loc, deriv_axes=[None,], deriv_method = autograd, nn_output=0)
+
+    u_var_non_neg = control_utils.ControlConstrNEq(val = torch.full_like(input = t, fill_value = 0., device=device), sign='>',
+                                                   indices = loc, deriv_method = autograd, nn_output=0)
+    v_var_non_neg = control_utils.ControlConstrNEq(val = torch.full_like(input = t, fill_value = 0., device=device), sign='>',
+                                                   indices = loc, deriv_method = autograd, nn_output=1)
+    contr_non_neg = control_utils.ControlConstrNEq(val = torch.full_like(input = t, fill_value = 0., device=device), sign='>',
+                                                   indices = loc, deriv_method = autograd, nn_output=0)    
+
+    
+    loss = control_utils.ConditionalLoss([(1., y_tar_constr, 0),
+                                          (1., v_tar_constr, 0), 
+                                          (0.001, contr_constr, 1),
+                                          (10., u_var_non_neg, 0),
+                                          (10., v_var_non_neg, 0),
+                                          (10., contr_non_neg, 1)])
+    optimizer = control_utils.ControlExp(loss=loss, device=device)
+    
+    def get_ode_bop(key, var, term, grid_loc, value):
+        bop = epde.interface.solver_integration.BOPElement(axis = 0, key = key, term = term,
+                                                           power = 1, var = var)
+        if isinstance(grid_loc, float):
+            bop_grd_np = np.array([[grid_loc,]])
+            bop.set_grid(torch.from_numpy(bop_grd_np).type(torch.FloatTensor)).to(device)
+        elif isinstance(grid_loc, torch.Tensor):
+            bop.set_grid(grid_loc.reshape((1, 1)).type(torch.FloatTensor)) # What is the correct shape here?
+        else:
+            raise TypeError('Incorret value type, expected float or torch.Tensor.')
+        bop.values = torch.from_numpy(np.array([[value,]])).float().to(device)
+        return bop
+
+    bop_y = get_ode_bop('y', 0, [None], t[0, 0], u_init)
+    bop_dy = get_ode_bop('y', 0, [0,], t[0, 0], v_init)
+
+    optimizer.system = eq
+
+    optimizer.set_control_optim_params()
+
+    optimizer.set_solver_params(training_params = {'epochs': 200,})
+
+    state_nn, ctrl_net, ctrl_pred, hist = optimizer.train_pinn(bc_operators = [(bop_u(device=device), 0.001), 
+                                                                               (bop_v(device=device), 0.001)], 
+                                                               grids = [t,], n_control = 1., 
+                                                               state_net = state_nn_pretrained, 
+                                                               opt_params = [0.005, 0.9, 0.999, 1e-8],
+                                                               control_net = ctrl_nn_pretrained, epochs = 50,
+                                                               fig_folder = fig_folder)
+
+    return state_nn, ctrl_net, ctrl_pred, hist
 
 def prepare_trajectories(n_sample: int = 250, single_sample: bool = True):
     env_config = {'domain_name': "cartpole",