ad4340bc6eecae56815848ca9f5e6ecf73ead963
Mikhail Maslyaev <maslyaitis@gmail.com>
2024-11-05 11:53:37 +0300

    ODEInt version implementation

---
+++ epde/integrate/bop.py
@@ -0,0 +1,274 @@
+from typing import Union, List, Dict
+from types import FunctionType
+from functools import singledispatch
+
+import numpy as np
+import torch
+
+import epde.globals as global_var
+from epde.structure.main_structures import SoEq
+
+VAL_TYPES = Union[FunctionType, int, float, torch.Tensor, np.ndarray]
+
+
+def get_max_deriv_orders(system_sf: List[Union[Dict[str, Dict]]], variables: List[str] = ['u',]) -> dict:
+    def count_factor_order(factor_code, deriv_ax):
+        if factor_code is None or isinstance(factor_code, tuple):
+            return 0
+        else:
+            if isinstance(factor_code, list):
+                return factor_code.count(deriv_ax)
+            elif isinstance(factor_code, int):
+                return 1 if factor_code == deriv_ax else 0
+            else:
+                raise TypeError(f'Incorrect type of the input. Got {type(factor_code), factor_code}, expecting int or list')
+
+    @singledispatch
+    def get_equation_requirements(equation_sf, variables=['u',]):
+        raise NotImplementedError(
+            'Single-dispatch called in generalized form')
+
+    @get_equation_requirements.register
+    def _(equation_sf: dict, variables=['u',]) -> dict:  # dict = {u: 0}):
+        dim = global_var.grid_cache.get('0').ndim
+        if len(variables) == 1:
+            var_max_orders = np.zeros(dim)
+            for term in equation_sf.values():
+                if isinstance(term['pow'], list):
+                    for deriv_factor in term['term']:
+                        orders = np.array([count_factor_order(deriv_factor, ax) for ax
+                                            in np.arange(dim)])
+                        var_max_orders = np.maximum(var_max_orders, orders)
+                else:
+                    orders = np.array([count_factor_order(term['term'], ax) for ax
+                                        in np.arange(dim)])
+                    var_max_orders = np.maximum(var_max_orders, orders)
+            return {variables[0]: var_max_orders}
+        else:
+            var_max_orders = {var_key: np.zeros(dim) for var_key in variables}
+            for term_key, symb_form in equation_sf.items():
+                if isinstance(symb_form['var'], list):
+                    assert len(symb_form['term']) == len(symb_form['var'])
+                    for factor_idx, deriv_factor in enumerate(symb_form['term']):
+                        var_orders = np.array([count_factor_order(deriv_factor, ax) for ax
+                                                in np.arange(dim)])
+                        if isinstance(symb_form['var'][factor_idx], int):
+                            var_key = symb_form['var'][factor_idx] #- 1
+                        else:
+                            var_key = 0
+                            var_orders = 0 # Such tokens do not increase order of the DE
+                        var_max_orders[variables[var_key]] = np.maximum(var_max_orders[variables[var_key]],
+                                                                        var_orders)
+                elif isinstance(symb_form['var'], int):
+                    raise NotImplementedError()
+                    assert len(symb_form['term']) == 1
+                    for factor_idx, factor in enumerate([count_factor_order(symb_form['term'], ax) for ax
+                                                        in np.arange(dim)]):
+                        var_orders = np.array([count_factor_order(deriv_factor, ax) for ax
+                                                in np.arange(dim)])
+                        var_key = symb_form['var'][factor_idx]
+                        var_max_orders[var_key] = np.maximum(var_max_orders[var_key], var_orders)
+            return var_max_orders
+
+    @get_equation_requirements.register
+    def _(equation_sf: list, variables=['u',]):
+        raise NotImplementedError(
+            'TODO: add equation list form processing') 
+
+    eq_forms = []
+    for equation_form in system_sf:
+        eq_forms.append(get_equation_requirements(equation_form, variables))
+
+    max_orders = {var: np.maximum.accumulate([eq_list[var] for eq_list in eq_forms])[-1]
+                    for var in variables}  # TODO
+    return max_orders
+
+class BOPElement(object):
+    def __init__(self, axis: int, key: str, coeff: float = 1., term: list = [None], 
+                 power: Union[Union[List[int], int]] = 1, var: Union[List[int], int] = 1,
+                 rel_location: float = 0., device = 'cpu'):
+        self.axis = axis
+        self.key = key
+        self.coefficient = coeff
+        self.term = term
+        self.power = power
+        self.variables = var
+        self.location = rel_location
+        self.grid = None
+        
+        self.status = {'boundary_location_set': False,
+                       'boundary_values_set': False}
+        
+        self._device = device
+
+    def set_grid(self, grid: torch.Tensor):
+        self.grid = grid
+        self.status['boundary_location_set'] = True
+
+    @property
+    def operator_form(self):
+        form = {
+            'coeff': self.coefficient,
+            self.key: self.term,
+            'pow': self.power,
+            'var': self.variables
+        }
+        return self.key, form
+
+    @property
+    def values(self):
+        if isinstance(self._values, FunctionType):
+            assert self.grid_set, 'Tring to evaluate variable coefficent without a proper grid.'
+            res = self._values(self.grids)
+            assert res.shape == self.grids[0].shape
+            return torch.from_numpy(res).to(self._device)
+        else:
+            return self._values
+
+    @values.setter
+    def values(self, vals):
+        if isinstance(vals, (FunctionType, int, float, torch.Tensor)):
+            self._values = vals
+            self.vals_set = True
+        elif isinstance(vals, np.ndarray):
+            self._values = torch.from_numpy(vals).to(self._device)
+            self.vals_set = True
+        else:
+            raise TypeError(
+                f'Incorrect type of coefficients. Must be a type from list {VAL_TYPES}.')
+
+    def __call__(self, values: VAL_TYPES = None) -> dict:
+        if not self.vals_set and values is not None:
+            self.values = values
+            self.status['boundary_values_set'] = True
+        elif not self.vals_set and values is None:
+            raise ValueError('No location passed into the BOP.')
+        if self.grid is not None:
+            boundary = self.grid
+        elif self.grid is None and self.location is not None:
+            _, all_grids = global_var.grid_cache.get_all(mode = 'torch')
+
+            abs_loc = self.location * all_grids[0].shape[self.axis]
+            if all_grids[0].ndim > 1:
+                boundary = np.array(all_grids[:self.axis] + all_grids[self.axis+1:])
+                if isinstance(values, FunctionType):
+                    raise NotImplementedError  # TODO: evaluation of BCs passed as functions or lambdas
+                boundary = torch.from_numpy(np.expand_dims(boundary, axis=self.axis)).to(self._device).float()
+
+                boundary = torch.cartesian_prod(boundary,
+                                                torch.from_numpy(np.array([abs_loc,], dtype=np.float64)).to(self._device)).float()
+                boundary = torch.moveaxis(boundary, source=0, destination=self.axis).resize()
+            else:
+                boundary = torch.from_numpy(np.array([[abs_loc,],])).to(self._device).float() # TODO: work from here
+            
+        elif boundary is None and self.location is None:
+            raise ValueError('No location passed into the BOP.')
+            
+        form = self.operator_form
+        boundary_operator = {form[0]: form[1]}
+        
+        boundary_value = self.values
+        
+        return {'bnd_loc' : boundary.to(self._device), 'bnd_op' : boundary_operator, 
+                'bnd_val' : boundary_value.to(self._device), 
+                'variables' : self.variables, 'type' : 'operator'}
+
+class PregenBOperator(object):
+    def __init__(self, system: SoEq, system_of_equation_solver_form: list): #, device = 'cpu'
+        self.system = system
+        self.equation_sf = [eq for eq in system_of_equation_solver_form]
+        self.variables = list(system.vars_to_describe)
+
+    def demonstrate_required_ords(self):
+        linked_ords = list(zip([eq.main_var_to_explain for eq in self.system],
+                                self.max_deriv_orders))
+
+    @property
+    def conditions(self):
+        return self._bconds
+
+    @conditions.setter
+    def conditions(self, conds: List[BOPElement]):
+        self._bconds = []
+        if len(conds) != int(sum([value.sum() for value in self.max_deriv_orders.values()])):
+            raise ValueError(
+                'Number of passed boundry conditions does not match requirements of the system.')
+        for condition in conds:
+            if isinstance(condition, BOPElement):
+                self._bconds.append(condition())
+            else:
+                print('condition is ', type(condition), condition)
+                raise NotImplementedError(
+                    'In-place initialization of boundary operator has not been implemented yet.')
+
+    @property
+    def max_deriv_orders(self):
+        return get_max_deriv_orders(self.equation_sf, self.variables)
+
+    def generate_default_bc(self, vals: Union[np.ndarray, dict] = None, grids: List[np.ndarray] = None,
+                            allow_high_ords: bool = False):
+        # Implement allow_high_ords - selection of derivatives from
+        required_bc_ord = self.max_deriv_orders
+        assert set(self.variables) == set(required_bc_ord.keys()), 'Some conditions miss required orders.'
+
+        grid_cache = global_var.initial_data_cache
+        tensor_cache = global_var.initial_data_cache
+
+        if vals is None:
+            val_keys = {key: (key, (1.0,)) for key in self.variables}
+
+        if grids is None:
+            _, grids = grid_cache.get_all(mode = 'torch')
+
+        device = global_var.grid_cache._device
+        # assert self._device
+        device_on_cpu = (device  == 'cpu')
+        relative_bc_location = {0: (), 1: (0,), 2: (0, 1),
+                                3: (0., 0.5, 1.), 4: (0., 1/3., 2/3., 1.)}
+
+        bconds = []
+        tensor_shape = grids[0].shape
+
+        def get_boundary_ind(tensor_shape, axis, rel_loc):
+            return tuple(np.meshgrid(*[np.arange(shape) if dim_idx != axis else min(int(rel_loc * shape), shape-1)
+                                       for dim_idx, shape in enumerate(tensor_shape)], indexing='ij'))
+
+        for var_idx, variable in enumerate(self.variables):
+            for ax_idx, ax_ord in enumerate(required_bc_ord[variable]):
+                for loc in relative_bc_location[ax_ord]:
+                    indexes = get_boundary_ind(tensor_shape, ax_idx, rel_loc=loc)
+
+                    if device_on_cpu:
+                        coords = np.array([grids[idx][indexes].detach().numpy() for idx in np.arange(len(tensor_shape))]).T
+                    else:
+                        coords = np.array([grids[idx][indexes].detach().cpu().numpy()
+                                           for idx in np.arange(len(tensor_shape))]).T
+                    if coords.ndim > 2:
+                        coords = coords.squeeze()
+
+                    if vals is None:
+                        bc_values = tensor_cache.get(val_keys[variable])[indexes]
+                    else:
+                        bc_values = vals[indexes]
+
+                    bc_values = np.expand_dims(bc_values, axis=0).T
+                    coords = torch.from_numpy(coords).to(device).float()
+
+                    bc_values = torch.from_numpy(bc_values).to(device).float() # TODO: set devices for all torch objs
+                    operator = BOPElement(axis=ax_idx, key=variable, coeff=1, term=[None],
+                                          power=1, var=var_idx, rel_location=loc, device=device)
+                    operator.set_grid(grid=coords)
+                    operator.values = bc_values
+                    bconds.append(operator)
+        self.conditions = bconds
+
+
+class BoundaryConditions(object):
+    def __init__(self, grids=None, partial_operators: dict = []):
+        self.grids_set = (grids is not None)
+        if grids is not None:
+            self.grids = grids
+        self.operators = partial_operators
+
+    def form_operator(self):
+        return [list(bcond()) for bcond in self.operators.values()]
---
+++ epde/integrate/odeint_integration.py
@@ -0,0 +1,254 @@
+from typing import Union, List, Dict, Tuple, Callable
+import copy
+from functools import reduce
+
+import torch
+import numpy as np
+from scipy.integrate import ode, solve_ivp
+
+from epde.solver.data import Domain, Conditions
+from epde.structure.main_structures import SoEq
+from epde.integrate.bop import PregenBOperator, BOPElement, get_max_deriv_orders
+from epde.integrate.interface import SystemSolverInterface
+
+def get_terms_der_order(equation: Dict, variable_idx: int) -> np.ndarray:
+    '''
+    Get the highest orders of the ``variable_idx``-th variable derivative in the equation terms.
+    '''
+    term_max_ord = np.zeros(len(equation))
+    for term_idx, term_dict in enumerate(equation.values()):
+        # print(term_dict['term'])
+        if isinstance(term_dict['var'], list) and len(term_dict['var']) > 1:
+            max_ord = 0
+            for arg_idx, deriv_ord in enumerate(term_dict['term']):
+                if isinstance(term_dict['pow'][arg_idx], int) and term_dict['var'][arg_idx] == variable_idx:
+                    max_ord = max(max_ord, len([var for var in deriv_ord if var is not None]))
+            term_max_ord[term_idx] = max_ord
+        elif isinstance(term_dict['var'], int):
+            if isinstance(term_dict['pow'], int) and term_dict['var'] == variable_idx:
+                term_max_ord[term_idx] = max(0, len([var for var in term_dict['term'] if var is not None]))
+        elif isinstance(term_dict['var'], list) and len(term_dict['var']) == 1:
+            if isinstance(term_dict['pow'], int) and term_dict['var'][0] == variable_idx:
+                term_max_ord[term_idx] = max(0, len([var for var in term_dict['term'] if var is not None]))
+
+    return term_max_ord
+
+def get_higher_order_coeff(equation: Dict, orders: np.ndarray, var: int) -> Tuple[List]:
+    def transform_term(term: Dict, deriv_key: list, var: int) -> Dict:
+        term_filtered = copy.deepcopy(term)
+        if (isinstance(term['var'], int) and term['var'] == var) or (isinstance(term['var'], list) 
+                                                                     and len(term['var']) == 1 and term['var'][0] == var):
+            term_filtered['term'] = [None,]
+            term_filtered['pow'] = 0
+        else:
+            term_idx = [der_var for idx, der_var in enumerate(term_filtered['term']) 
+                        if der_var == deriv_key and term_filtered['pow'][idx] == var][0]
+            term_filtered['term'][term_idx] = [None,]
+            term_filtered['pow'][term_idx] = 0
+        return term_filtered            
+
+    denom_terms = []
+    numer_terms = []
+    for term_idx, term in enumerate(equation.values()):
+        if orders[term_idx] == np.max(orders):
+            denom_terms.append(transform_term(term, deriv_key=[0,]*int(np.max(orders)), var=var))
+        else:
+            numer_terms.append(term)
+    return [denom_terms, numer_terms]
+
+def get_eq_order(equation, variables: List[str]):
+    eq_var = 0; eq_orders = np.zeros(len(equation))
+    for var_idx in range(len(variables)):
+        orders = get_terms_der_order(equation=equation, variable_idx=var_idx)
+        if np.max(orders) > np.max(eq_orders):
+            eq_var = var_idx; eq_orders = orders
+    return eq_var, eq_orders
+
+def replace_operator(term: Dict, variables: List):
+    '''
+
+    Variables have to be in form of [(0, [None]), (0, [0,]), (0, [0, 0]), (0, [0, 0, 0]), (1, [None,]), ... ]
+    where the list elements are factors, taken as derivatives: (variable, differentiations), and the index in list
+    matches the index of dynamics operator output.
+
+    '''
+    term_ = copy.deepcopy(term)
+    if isinstance(term_['var'], list) and len(term_['var']) > 1:
+        for arg_idx, deriv_ord in enumerate(term_['term']):
+            term_['var'][arg_idx]  = variables.index((term_['var'][arg_idx], deriv_ord))
+            term_['term'][arg_idx] = [None,]
+    elif isinstance(term['var'], int) or (isinstance(term_['var'], list) and len(term_['var']) == 1):
+        if isinstance(term['var'], int):
+            term_var = term_['var']
+        else:
+            term_var = term_['var'][0]
+        if isinstance(term['pow'], int):
+            term_['var']  = variables.index((term_var, term_['term']))
+            term_['term'] = [None,]
+    return term_
+
+class ImplicitEquation(object):
+    def __init__(self, system: List, grid: np.ndarray, variables: List[str]):
+        self.grid_dict = grid
+
+        self._dynamics_operators = []
+        var_order = []
+        self._vars_with_eqs = {}
+
+        for var, order in [get_eq_order(equation, variables) for equation in system]:
+            var_order.extend([(var, [None,])] + [(var, [0,]*(idx+1)) for idx in range(int(np.max(order))-1)])
+            if len(self._vars_with_eqs) == 0:
+                self._vars_with_eqs[int(np.max(order)) - 1] = (var, order)
+            else:
+                self._vars_with_eqs[list(self._vars_with_eqs.keys())[-1] + int(np.max(order))] = (var, order)
+
+        for var_idx, var in enumerate(var_order):
+            if var_idx in self._vars_with_eqs.keys():
+                operator = get_higher_order_coeff(equation = system[self._vars_with_eqs[var_idx][0]],
+                                                  orders = self._vars_with_eqs[var_idx][1], 
+                                                  var = self.__vars_with_eqs[var_idx][0])
+                operator[0] = [replace_operator(denom_term, var_order) for denom_term in operator[0]]
+                operator[1] = [replace_operator(numer_term, var_order) for numer_term in operator[1]]
+            else:
+                operator = [None, self.create_first_ord_eq(var_idx + 1)]
+            self._dynamics_operators.append(operator)
+
+    def parse_cond(self, conditions: List[BOPElement]):
+        cond_val = np.full(shape = len(self._dynamics_operators), fill_value=np.inf)
+        for cond in conditions:
+            assert isinstance(cond.variables, int), 'Boundary operator has to contain only a single variable.'
+            try:
+                var = self._vars_with_eqs.index((cond.variables, cond.axis))
+            except ValueError:
+                print(f'Missing {cond.variables, cond.axis} from the list of variables {self._vars_with_eqs}')
+            cond_val[var] = cond.values
+        assert np.sum(np.inf == cond_val) == 0, 'Not enough initial conditions were passed.'
+        return cond_val
+
+    def __call__(self, t, y):
+        values = np.empty(len(self._dynamics_operators))
+        for idx, operator in enumerate(self._dynamics_operators):
+            if operator[0] is None:
+                denom = 1
+            else:
+                denom = [self.term_callable(term, t, y) for term in operator[0]]
+                if np.isclose(denom, 0):
+                    raise ZeroDivisionError('Denominator in the dynamics operator is close to zero.')
+            numerator = [self.term_callable(term, t, y) for term in operator[1]]
+            values[idx] = np.sum(numerator)/np.sum(denom)
+        return values
+
+    @property
+    def grid_dict(self):
+        return self._grid_rounded
+
+    @grid_dict.setter
+    def grid_dict(self, grid_points):
+        self._grid_step = grid_points[1] - grid_points[0]
+        digits = np.floor(np.log10(self._grid_step/2.)-1)
+        self._grid_rounded = {np.round(grid_val, -int(digits)): idx 
+                              for idx, grid_val in np.ndenumerate(grid_points)}
+
+    def create_first_ord_eq(self, var: int) -> List[Tuple]:
+        '''
+        Example of order: np.array([3., 0., 0.]) for third ord eq. 
+        '''
+        return [{'coeff' : 1., 
+                 'term'  : [None,],
+                 'pow'   : 1,
+                 'var'   : var},] # TODO: validate
+
+    def merge_coeff(self, coeff: np.ndarray, t: float):
+        try:
+            return self.grid_dict[t]
+        except KeyError:
+            for grid_loc, grid_idx in self.grid_dict.items():
+                if grid_loc < t and grid_loc + self._grid_step > t:
+                    print('Search in ', grid_loc, grid_loc + self._grid_step)
+                    left_loc, right_loc = grid_loc, grid_loc + self._grid_step
+                    left_idx, right_idx = grid_idx[0], grid_idx[0] + 1
+                    break
+            val = coeff[left_idx] + (t - left_loc) / (right_loc - left_loc) * (coeff[right_idx] - coeff[left_idx])
+            return val
+
+    def term_callable(self, term: Dict, t, y):
+        def call_ann_token(token_nn: torch.nn.Sequential, arguments: list,
+                           t: float, y: np.ndarray):
+            return token_nn[torch.from_numpy(y[tuple(arguments)])].detach().numpy() # Hereby, the ANN does not explicitly depend on time
+
+        if isinstance(term['coeff'], Callable):
+            k = term['coeff'](t)
+        elif isinstance(term['coeff'], torch.nn.Sequential):
+            k = term['coeff'](torch.from_numpy(t).reshape((1, 1).float()))
+        elif isinstance(term['coeff'], np.ndarray):
+            k = self.merge_coeff(term['coeff'], t)
+        else:
+            k = term['coeff']
+        if isinstance(term['var'], int):
+            if isinstance(term['pow'], int):
+                values = [y[term['var']]**term['pow'],]
+            elif isinstance(term['pow'], torch.nn.Sequential):
+                values = [call_ann_token(term['power'], term['var'], t, y),] #[term['power'](y[term['var']]).detach().numpy(),]
+            else:                
+                values = [term['pow'](y[term['var']]),]
+        elif isinstance(term['var'], list):
+            values = []
+            for var_idx, var in enumerate(term['var']):
+                if isinstance(term['pow'][var_idx], int):
+                    values.append(y[var]**term['pow'][var_idx])
+                elif isinstance(term['pow'][var_idx], torch.nn.Sequential):
+                    values.append(call_ann_token(term['power'][var_idx], term['var'][var_idx], t, y)) 
+                    # term['pow'][var_idx](y[var]).detach().numpy())
+                    # TODO: validate on LV eqs
+                else:                
+                    values.append(term['pow'][var_idx](y[var]))
+        return reduce(lambda x, z: x*z, values, k)
+
+
+class OdeintAdapter(object):
+    def __init__(self, method: str = 'Radau'):
+        self._solve_method = method
+        pass # TODO: implement hyperparameters setup, according to the problem specifics
+
+    def solve_epde_system(self, system: Union[SoEq, dict], grids: list=None, boundary_conditions=None,
+                          mode='NN', data=None, use_cache: bool = False, use_fourier: bool = False,
+                          fourier_params: dict = None, use_adaptive_lambdas: bool = False,
+                          to_numpy: bool = False, *args, **kwargs):
+        if isinstance(system, SoEq):
+            system_interface = SystemSolverInterface(system_to_adapt=system)
+            system_solver_forms = system_interface.form(grids = grids, mode = mode)
+        elif isinstance(system, List[Dict]):
+            system_solver_forms = system
+        
+        if boundary_conditions is None:
+            op_gen = PregenBOperator(system=system,
+                                     system_of_equation_solver_form=[sf_labeled[1] for sf_labeled
+                                                                     in system_solver_forms])
+            op_gen.generate_default_bc(vals = data, grids = grids)
+            boundary_conditions = op_gen.conditions
+            
+        bconds_combined = Conditions()
+        for cond in boundary_conditions:
+            bconds_combined.operator(bnd = cond['bnd_loc'], operator = cond['bnd_op'], 
+                                     value = cond['bnd_val'])        
+
+        return self.solve(equations = system_solver_forms, domain = grids[0], 
+                          boundary_conditions = bconds_combined, vars = system.vars_to_describe)
+
+        # Add condition parser and control function args parser
+
+
+
+    def solve(self, equations: SoEq, domain: Union[Domain, np.ndarray], boundary_conditions: List[BOPElement] = None,
+              mode = 'NN', use_cache: bool = False, use_fourier: bool = False, fourier_params: dict = None, 
+              use_adaptive_lambdas: bool = False, to_numpy = False, vars: List['str'] = ['x',], *args, **kwargs):
+        if not isinstance(equations, SoEq):
+            raise RuntimeError('Incorrect type of equations passed into odeint solver.')
+        self._implicit_equation = ImplicitEquation(equations, domain, equations.vars)
+        grid = domain.build().detach().numpy().reshape(-1)
+        print(f'grid[0] {grid[0]}, grid[-1] {grid[-1]}')
+
+        initial_cond = self._implicit_equation.parse_cond(boundary_conditions)
+        solution = solve_ivp(fun = self._implicit_equation, t_span = (grid[0], grid[-1]), y0=initial_cond,
+                             t_eval = grid, method = self._solve_method)
+        return 0, solution
