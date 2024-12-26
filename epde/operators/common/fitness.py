0245bfabd39065cab0bc775752f0a9d453ab068c
Gromwud <timur.bavshin@gmail.com>
2024-12-26 16:45:51 +0300

    fixed statistics for r-loss

--- epde/operators/common/fitness.py
+++ epde/operators/common/fitness.py
@@ -264,7 +264,7 @@ class PIC(CompoundOperator):
                 target_window = target_vals[start_idx:end_idx]
                 feature_window = features[start_idx:end_idx, :]
 
-                # # Initial guess for parameters
+                #   # Initial guess for parameters
                 # initial_params = np.zeros(feature_window.shape[1])
                 #
                 # # Optimize weights for the current window
@@ -272,8 +272,7 @@ class PIC(CompoundOperator):
                 #                   method='L-BFGS-B')
                 #
                 # if result.success:
-                #     window_weights = result.x
-                #     eq_window_weights.append(window_weights)
+                #     valuable_weights = result.x
 
                 # Linear Regression
                 estimator = LinearRegression(fit_intercept=False)
@@ -285,6 +284,7 @@ class PIC(CompoundOperator):
                     self.g_fun_vals_window = None
                 estimator.fit(feature_window, target_window, sample_weight=self.g_fun_vals_window)
                 valuable_weights = estimator.coef_
+
                 window_weights = np.zeros(len(eq.structure))
                 for weight_idx in range(len(window_weights) - 1):
                     if weight_idx in nonzero_features_indexes:
@@ -293,10 +293,11 @@ class PIC(CompoundOperator):
                 eq_window_weights.append(window_weights)
 
             # eq_cv = [np.std(_) / np.mean(_) for _ in zip(*eq_window_weights)]  # Default std
-            eq_cv = [np.abs(np.std(_) / (np.mean(_) + 1e-8)) for _ in zip(*eq_window_weights)]  # As in papers' repo
+            eq_cv = [np.abs(np.std(_) / (np.mean(_))) for _ in zip(*eq_window_weights)]  # As in papers' repo
             # eq_cv = [np.mean((_ - np.mean(_)) / np.mean(_)) for _ in zip(*eq_window_weights)]  # As in paper formula (BUG)
-            lr = np.mean(eq_cv)
-            print(eq_cv)
+
+            eq_cv_valuable = [x for x in eq_cv if not np.isnan(x)]
+            lr = np.mean(eq_cv_valuable)
 
             # Calculate p-loss
             if torch.isnan(loss_add):
